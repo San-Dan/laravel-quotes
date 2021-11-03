bdf49c057a80caac30d68a46c7a2ee1d29d5e7d9
San-Dan <sandradanielsson94@hotmail.com>
2021-11-03 19:25:02 +0100

    laravel breeze kit for authentication

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-
-Route::resource('/card', 'CardController');
-
-Route::get('/collection/{collection}/cards', 'CollectionController@cards');
-Route::resource('/collection', 'CollectionController');
